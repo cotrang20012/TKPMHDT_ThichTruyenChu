b593e6401e7175641d0ad7c283d12c0afb6d708e
Hoàng Đức <hoanghuuduc22@gmail.com>
2022-05-19 12:15:04 +0700

    Update comment,saved

---
+++ src/main/java/mobile/repository/CommentRepository.java
@@ -0,0 +1,20 @@
+package mobile.repository;
+
+import mobile.model.Entity.Comment;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.List;
+import java.util.Optional;
+
+@EnableMongoRepositories
+public interface CommentRepository extends MongoRepository<Comment, ObjectId> {
+    void deleteAllByUrltruyen(String urlTruyen);
+    List<Comment> findAllByUrltruyenAndParentIdIsNull(String urlTruyen, Pageable pageable);
+    List<Comment> findAllByParentId(ObjectId parentId, Pageable pageable);
+    List<Comment> findAllByParentId(ObjectId parentId);
+    Boolean existsByParentId(ObjectId parentId);
+    Optional<Comment> findById(ObjectId objectId);
+}
